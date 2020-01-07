7121a964ad36655c700c427437f005c1cc0aca7e
luke scheller <lukescheller1991@outlook.com>
2020-01-06 21:35:00 -0800

    COMMITING

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Jumbotron, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Skills from './Skills'
 import TrainerStats from './TrainerStats'
-import Banner from '../images/banner.jpg'
+import Banner from '../images/banner.png'
 import pd from '../images/pokedex.png'
 import ig from '../images/ig.png'
 import git from '../images/git.png'
@@ -12,18 +12,13 @@ const Home = () => {
     return (
         <Container>
             <Jumbotron style={style} fluid='false'>
-                <Row style={{ opacity: '0.2' }}>
-                    <Col style={{ fontSize: '80px', fontFamily: 'VT323' }}>Luke Scheller</Col>
-                </Row>
-                <Row style={{ opacity: '0.2' }}>
-                    <Col style={{ fontFamily: 'VT323' }}><i><h4 style={{ fontFamily: 'VT323' }}>Designer & Developer</h4></i></Col>
-                </Row>
-                <Row style={{ border: '1px solid none', display: 'flex', justifyContent: 'flex-start', position: 'relative', top: '140px' }}>
-                    <Col style={{ border: '1px solid none' }}><a href='https://www.instagram.com/_the_meme_mayor_/'><img src={ig} alt='' width='70px' /></a></Col>
-                    <Col style={{ border: '1px solid none' }}><Link to='/pokedex'><img src={pd} alt='' width='100px' style={{ position: 'relative', bottom: '10px' }} /></Link></Col>
-                    <Col style={{ border: '1px solid none' }}><a href='https://github.com/lukescheller'><img src={git} alt='' width='70px' /></a></Col>
-                </Row>
             </Jumbotron>
+
+            <Row style={{ border: '1px solid none', display: 'flex', justifyContent: 'flex-start' }}>
+                <Col style={{ border: '1px solid none' }}><a href='https://www.instagram.com/_the_meme_mayor_/'><img src={ig} alt='' width='70px' /></a></Col>
+                <Col style={{ border: '1px solid none' }}><Link to='/pokedex'><img src={pd} alt='' width='100px' style={{ position: 'relative', bottom: '10px' }} /></Link></Col>
+                <Col style={{ border: '1px solid none' }}><a href='https://github.com/lukescheller'><img src={git} alt='' width='70px' /></a></Col>
+            </Row>
 
             <Jumbotron style={{ backgroundColor: '#edfafc' }}>
                 <h1 style={{ fontWeight: 'bold' }}>Welcome!</h1>
@@ -34,12 +29,12 @@ const Home = () => {
             </Jumbotron>
 
             <div style={skTitle}>
-                <h1>Trainer Profile</h1>
+                <h1 style={{ marginBottom: '20px' }}>Trainer Profile</h1>
             </div>
             <TrainerStats />
 
             <div style={skTitle}>
-                <h1>Gym Badges</h1>
+                <h1 style={{ marginBottom: '20px' }}>Gym Badges</h1>
             </div>
             <Skills />
 
