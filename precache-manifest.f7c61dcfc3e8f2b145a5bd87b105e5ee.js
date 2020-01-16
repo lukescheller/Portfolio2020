3f00d763f543d4d6c5ee6955481de068f52e30ae
luke scheller <lukescheller1991@outlook.com>
2020-01-16 14:44:22 -0800

    Updates

---
+++ precache-manifest.f7c61dcfc3e8f2b145a5bd87b105e5ee.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffcec5d1642dc1d90f04519f0813ab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ba1dd73fa7fb811372",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d0b9d67988d8663ed147",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2ba1dd73fa7fb811372",
+    "url": "/static/js/2.668248c4.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.668248c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0b9d67988d8663ed147",
+    "url": "/static/js/main.ebec8704.chunk.js"
+  },
+  {
+    "revision": "8a017ea3e65493366ed2",
+    "url": "/static/js/runtime-main.b2eb7556.js"
+  },
+  {
+    "revision": "45e21c663a35c12f61d5df2e267583a4",
+    "url": "/static/media/JAVASCRIPT.45e21c66.jpg"
+  },
+  {
+    "revision": "7f24cb27e40fa2999020872c1c06e9a8",
+    "url": "/static/media/NODEJS.7f24cb27.jpg"
+  },
+  {
+    "revision": "9195664adcdc8e88fcfca529776a643d",
+    "url": "/static/media/banner.9195664a.png"
+  },
+  {
+    "revision": "4f040df0975bd54c6030a27ccf90143b",
+    "url": "/static/media/cropped.4f040df0.png"
+  },
+  {
+    "revision": "c4f4a481e26b7e2716674d7b2a9f2071",
+    "url": "/static/media/git.c4f4a481.png"
+  },
+  {
+    "revision": "df711cec5a533c820184f87dd8e6e7de",
+    "url": "/static/media/html.df711cec.png"
+  },
+  {
+    "revision": "cd49d8b89cf14e93c41e845658d95fe8",
+    "url": "/static/media/ig.cd49d8b8.png"
+  },
+  {
+    "revision": "cfe03380036209b2b812abbff0898852",
+    "url": "/static/media/map.cfe03380.png"
+  },
+  {
+    "revision": "5aeb7583cbacd14bdcb86101a1476376",
+    "url": "/static/media/pg.5aeb7583.png"
+  },
+  {
+    "revision": "8d1dc3198dd0fc77f0808bb81cc459d3",
+    "url": "/static/media/pokedex.8d1dc319.png"
+  },
+  {
+    "revision": "d8aeb6c192463ff6d03a64421ff2f550",
+    "url": "/static/media/pxbl.d8aeb6c1.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  }
+]);
